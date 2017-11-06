be39d4933e10f024a65522cede638f2ce50cb1ab
Luca Fabbri <luca@keul.it>
2017-11-06 12:27:56 +0100

    Can now act on sub-components

--- README.md
+++ README.md
@@ -1,11 +1,125 @@
-# react-selectall library
+react-selectall
+===============
 
 [![Build Status](https://travis-ci.org/keul/react-selectall.svg?branch=master)](https://travis-ci.org/keul/react-selectall)
-[![Dependencies](https://img.shields.io/keul/react-selectall/svg)]()
-[![Dev Dependencies](https://img.shields.io/keul/react-selectall.svg)]()
+[![Dependencies](https://img.shields.io/david/keul/react-selectall/svg)]()
+[![Dev Dependencies](https://img.shields.io/david/dev/keul/react-selectall.svg)]()
 
-Component for select text easily in ReactJS.
+ReactJS component for selecting text easily.
 
-## Demo app
+Demo
+----
 
-Is located inside `src/demo` directory, here you can test your library while developing.
+A demo is located inside `src/demo` directory, here you can test your library while developing.
+
+Tu run the demo locally, clone the repository and type:
+
+```
+npm install
+npm run
+```
+
+Then open `localhost:8000` in a browser.
+
+Installation
+------------
+
+Install from npm:
+
+```
+npm install react-selectall --save
+```
+
+Usage
+-----
+
+The simplest use is to wrap your subcomponents or subelements inside a `SelectAll` container:
+
+```jsx
+import SelectAll from 'react-selectall';
+
+<SelectAll>
+    <div>Foo bar baz</div>
+    <CustomComponent>qux tux</CustomComponent>
+</SelectAll>
+```
+
+When you will double-click on the rendered container, all of the text inside will be selected.
+
+Change activation event
+~~~~~~~~~~~~~~~~~~~~~~~
+
+By default the select-all activation event is a double-click.
+
+You can change it to a single click customizing the `selectOn`:
+
+```jsx
+import SelectAll from 'react-selectall';
+
+<SelectAll selectOn="onClick">
+    <div>Foo bar baz</div>
+    <CustomComponent>qux tux</CustomComponent>
+</SelectAll>
+```
+
+Copy to clipboard
+~~~~~~~~~~~~~~~~~
+
+The most common usage of a selected text is to copy it.
+
+You can save the user from doing the copy operation itself by activating the `toClipboard` prop:
+
+```jsx
+import SelectAll from 'react-selectall';
+
+<SelectAll toClipboard>
+    <div>Foo bar baz</div>
+    <CustomComponent>qux tux</CustomComponent>
+</SelectAll>
+```
+
+**Please note**: copy to clipboard can not be supported by every browser.
+
+Plus: some versions of Internet Explorer ask explicitly a confirmation to the user.
+
+Copy event
+~~~~~~~~~~
+
+Still you can be interested in doing other operations with the selected text.
+
+You can then pass an event handler function to the `onSelect: prop
+
+```jsx
+import SelectAll from 'react-selectall';
+
+<SelectAll onSelect={handleSelect}>
+    <div>Foo bar baz</div>
+    <CustomComponent>qux tux</CustomComponent>
+</SelectAll>
+```
+
+The `handleSelect` receive following parameters:
+
+* `ev` - the event who triggered the action
+* `text` - the selected text
+* `toClipboard` - a boolean value, true if the selection has been successfully copied to clipboard.
+
+Subelements
+-----------
+
+By default a `SelectAll` component wrap what it contains in a single selection block.
+
+But You can move the selection behavior to childrens:
+
+```jsx
+import SelectAll from 'react-selectall';
+
+<SelectAll childSelection>
+    <div>Foo bar baz</div>
+    <CustomComponent>qux tux</CustomComponent>
+</SelectAll>
+```
+
+In that case the selection take place when acting on DOM nodes contained inside the `SelectAll` component.
+
+Please note: this will also affects other properties like `onSelect`, `selectOn`, `toClipboard`.
--- src/demo/App.js
+++ src/demo/App.js
@@ -11,15 +11,24 @@ const writeTo = text => {
   }, 5000);
 };
 
+const CustomP = props => {
+  const { children, ...other } = props;
+  return <p {...other}>{children}</p>;
+};
+
+const CustomSpan = props => {
+  const { children, ...other } = props;
+  return <span {...other}>{children}</span>;
+};
+
+
 const App = () => (
   <div>
     <h1>
       <span>Getting started</span>
     </h1>
-    <p className="doc">
-      Select the text below with a double-click.
-    </p>
-    <SelectAll className="selectable">
+    <p className="doc">Select the text below with a double-click.</p>
+    <SelectAll className="demo selectable">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur
         lacus feugiat tellus condimentum, eget lobortis mauris efficitur. Sed
@@ -50,7 +59,7 @@ const App = () => (
       <span>Just click</span>
     </h1>
     <p className="doc">A simple single-click trigger the selection.</p>
-    <SelectAll className="selectable" selectOn="onClick">
+    <SelectAll className="demo selectable" selectOn="onClick">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur
         lacus feugiat tellus condimentum, eget lobortis mauris efficitur. Sed
@@ -84,7 +93,7 @@ const App = () => (
       You can add an event handler to executed after selection take place.
     </p>
     <SelectAll
-      className="selectable"
+      className="demo selectable"
       onSelect={(ev, text, toClipboard) => {
         writeTo(`You selected some text!
 
@@ -103,13 +112,7 @@ ${text}
         pharetra at diam non semper. Mauris vel nisi ligula. Donec tempor vel
         lorem vel aliquet. Etiam faucibus posuere massa at consectetur. Etiam
         interdum turpis sed nibh viverra, in porttitor justo tristique.
-        Phasellus ac felis in ligula aliquam sollicitudin. Sed arcu justo,
-        laoreet a sagittis ultricies, elementum et arcu. Proin hendrerit lectus
-        ac pellentesque ornare. Pellentesque congue faucibus lacus egestas
-        hendrerit. Suspendisse accumsan elit non elit iaculis condimentum. Cras
-        erat nisi, ultrices efficitur lacinia vel, dictum eu velit. Aliquam ac
-        blandit turpis, sed hendrerit orci. Cras scelerisque, leo eu maximus
-        vulputate, quam sapien euismod nunc, sed euismod metus quam a turpis.
+        Phasellus ac felis in ligula aliquam sollicitudin.
       </p>
       <p>
         Etiam ligula ex, cursus in ante sit amet, facilisis pretium tortor. Ut
@@ -117,8 +120,7 @@ ${text}
         primis in faucibus. Maecenas sapien nulla, posuere eget ligula eu,
         consequat accumsan enim. Aenean ullamcorper aliquam magna a scelerisque.
         Sed a ante vel velit pulvinar aliquam a a lorem. Pellentesque sit amet
-        mauris venenatis enim placerat consectetur. Maecenas commodo varius quam
-        vel tristique. In at tincidunt libero.
+        mauris venenatis enim placerat consectetur.
       </p>
     </SelectAll>
 
@@ -129,7 +131,7 @@ ${text}
       You can enable automatic copy to clipboard feature. Selected text is
       automatically copied.
     </p>
-    <SelectAll className="selectable" toClipboard>
+    <SelectAll className="demo selectable" toClipboard>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur
         lacus feugiat tellus condimentum, eget lobortis mauris efficitur. Sed
@@ -144,6 +146,89 @@ ${text}
         primis in faucibus. Maecenas sapien nulla, posuere eget ligula eu,
         consequat accumsan enim. Aenean ullamcorper aliquam magna a scelerisque.
         Sed a ante vel velit pulvinar aliquam a a lorem.
+      </p>
+    </SelectAll>
+
+    <h1>
+      <span>Act on subelements</span>
+    </h1>
+    <p className="doc">
+      You can choose to trigger selection when acting on sub-elements.{' '}
+      <em><small>Works on React subcomponents as well.</small></em>
+    </p>
+    <SelectAll className="demo" childSelection>
+      <p className="selectable">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur
+        lacus feugiat tellus condimentum, eget lobortis mauris efficitur. Sed
+        pharetra at diam non semper. Mauris vel nisi ligula.
+      </p>
+      <p className="selectable">
+        Elit pede donec, vel at, vitae mi. In wisi felis. Duis blandit libero.
+        Magnis penatibus dolor, nulla porttitor ut, facilisi elementum. Orci
+        ultrices, cum diam dolor, nibh ipsum. Ac vestibulum, non tortor integer,
+        sit accumsan. Eget ut est, sint commodo est, pellentesque felis. Erat
+        id, eget elementum.
+      </p>
+      <CustomP style={{fontStyle: 'italic'}}>
+        <CustomSpan>
+          Eleifend ac a, laoreet morbi leo. In pretium wisi, volutpat et risus, id
+          diam. Id sagittis quam. Eros proin, viverra at. Nec morbi. Dapibus
+          rutrum, at nam. Metus sed amet. Convallis metus a, sem curabitur nam.
+          Molestie delectus temporibus, iaculis quisque nunc, per fusce. Dolor
+          placerat, nec lorem ullamcorper.
+        </CustomSpan>
+      </CustomP>
+      <p className="selectable">
+        Etiam ligula ex, cursus in ante sit amet, facilisis pretium tortor. Ut
+        mollis malesuada dui ut porta. Interdum et malesuada fames ac ante ipsum
+        primis in faucibus. Maecenas sapien nulla, posuere eget ligula eu,
+        consequat accumsan enim.
+      </p>
+    </SelectAll>
+
+    <h1>
+      <span>Acting on subelements supports other features too</span>
+    </h1>
+    <p className="doc">
+      Event activation, handlers and other props are supported by sub-elements too.
+    </p>
+    <SelectAll className="demo" childSelection onSelect={(ev, text, toClipboard) => {
+      writeTo(`You selected some text!
+
+Triggered by: ${ev.type} event.
+Copied to clipboard: ${toClipboard ? 'true' : 'false'}
+
+Text selected:
+
+${text}
+`);
+    }} selectOn="onClick" toClipboard>
+      <p className="selectable">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur
+        lacus feugiat tellus condimentum, eget lobortis mauris efficitur. Sed
+        pharetra at diam non semper. Mauris vel nisi ligula.
+      </p>
+      <p className="selectable">
+        Elit pede donec, vel at, vitae mi. In wisi felis. Duis blandit libero.
+        Magnis penatibus dolor, nulla porttitor ut, facilisi elementum. Orci
+        ultrices, cum diam dolor, nibh ipsum. Ac vestibulum, non tortor integer,
+        sit accumsan. Eget ut est, sint commodo est, pellentesque felis. Erat
+        id, eget elementum.
+      </p>
+      <CustomP style={{fontStyle: 'italic'}}>
+        <CustomSpan>
+          Eleifend ac a, laoreet morbi leo. In pretium wisi, volutpat et risus, id
+          diam. Id sagittis quam. Eros proin, viverra at. Nec morbi. Dapibus
+          rutrum, at nam. Metus sed amet. Convallis metus a, sem curabitur nam.
+          Molestie delectus temporibus, iaculis quisque nunc, per fusce. Dolor
+          placerat, nec lorem ullamcorper.
+        </CustomSpan>
+      </CustomP>
+      <p className="selectable">
+        Etiam ligula ex, cursus in ante sit amet, facilisis pretium tortor. Ut
+        mollis malesuada dui ut porta. Interdum et malesuada fames ac ante ipsum
+        primis in faucibus. Maecenas sapien nulla, posuere eget ligula eu,
+        consequat accumsan enim.
       </p>
     </SelectAll>
   </div>
